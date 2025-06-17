aa0a620ee4d4097c8061e49b9d30829962eef3e0
james vundi <vkambuni@gmail.com>
2025-06-17 13:59:35 +0300

    Add carts table with user foreign key

--- database/migrations/2025_04_26_170856_create_cart_table.php
+++ database/migrations/2025_04_26_170856_create_cart_table.php
@@ -9,19 +9,24 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+ public function up(): void
+{
+    if (!Schema::hasTable('carts')) {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->nullable();
             $table->string('session_id')->nullable();
             $table->timestamps();
-            
-            // $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
         });
     }
+}
+
+
 
     /**
      * Reverse the migrations.
